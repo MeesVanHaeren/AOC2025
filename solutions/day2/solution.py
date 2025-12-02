1f3211b2f6d893b3ae9d86cc9a1f316f27774a97
Mees <m.vanhaeren@student.han.nl>
2025-12-02 23:26:01 +0100

    Day 2 silver star

--- solutions/day2/solution.py
+++ solutions/day2/solution.py
@@ -2,5 +2,32 @@
 
 import sys
 
-def isInvalid(numberString):
-    return len(numberString) % 2 != 0 or numberString[:int(len(numberString)/2)] == numberString[int(len(numberString)/2):]
+def isValid(numberString):
+    if len(numberString) % 2 != 0:
+        return True
+    
+    return numberString[:int(len(numberString)/2)] != numberString[int(len(numberString)/2):]
+
+def getInvalidIds(low, high):
+    i = low
+    invalidIds = []
+
+    while i <= high:
+        if not isValid(str(i)):
+            invalidIds.append(i)
+        i += 1
+    return invalidIds
+
+print("Day 2 solution:")
+
+rangeList = sys.stdin.read().strip().split(",")
+
+range2dList = list(map(lambda x: x.split("-"),rangeList))
+
+sumIds = 0
+
+for _range in range2dList:
+    for _id in getInvalidIds(int(_range[0]),int(_range[1])):
+        sumIds += _id
+
+print("Sum of invalid Ids: " + str(sumIds))
